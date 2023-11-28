36b39e8b58644af163efd3e5b23572fa79ea03f6
orieasy1 <easy1nhard2@gmail.com>
2023-11-28 22:19:32 +0900

    commit

--- R/BinomialnNormal.r
+++
@@ -1,6 +0,0 @@
-x <- 0:100
-y_binom <- dbinom(x, size=100, prob=0.6) #pmf of binomal
-y_normal <-dnorm(x, mean = 60, sd=sqrt(24)) #pdf of normal
-plot(x, y_binom, type="s", col="blue") #draw in stair steo
-par(new=TRUE)
-plot(x, y_normal, type="l", col="red") #draw in line
