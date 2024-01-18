292d9ba5c12fe9192046729afc4d007553637652
orieasy1 <easy1nhard2@gmail.com>
2024-01-19 03:54:07 +0900

    commit

--- sbb/src/main/java/com/mysite/sbb/HelloLombok.java
+++ sbb/src/main/java/com/mysite/sbb/HelloLombok.java
@@ -1,2 +1,20 @@
-package com.mysite.sbb;public class HelloLombok {
+package com.mysite.sbb;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@RequiredArgsConstructor
+@Getter
+public class HelloLombok {
+    //final 적용까지 완료하면 생성자를 직접 작성한 경우와 동일해짐
+    private final String hello;
+    private final int lombok;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok("헬로",5);
+
+        System.out.println(helloLombok.getHello());
+        System.out.println(helloLombok.getLombok());
+    }
 }
