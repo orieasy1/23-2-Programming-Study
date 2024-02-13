aac07121d822be3b165273edfb5c122f109b40bc
orieasy1 <easy1nhard2@gmail.com>
2024-02-13 19:51:39 +0900

    commit

---
+++ Java-Basic-Code/src/class1/ClassStart2.java
@@ -0,0 +1,18 @@
+package class1;
+
+public class ClassStart1 {
+    public static void main(String[] args) {
+        //이렇게 작성을 하면 학생 수가 늘어날 때 마다 코드가 길어지고 복잡해진다.
+        String student1Name = "학생1";
+        int student1Age = 15;
+        int student1Grade = 90;
+
+        String student2Name = "학생2";
+        int student2Age = 16;
+        int student2Grade = 80;
+
+        System.out.println("이름:" + student1Name + " 나이:" + student1Age + " 성적:" + student1Grade);
+        System.out.println("이름:" + student2Name + " 나이:" + student2Age + " 성적:" + student2Grade);
+
+    }
+}
