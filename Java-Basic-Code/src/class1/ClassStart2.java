60cfc56d3d8d91126491ebabcf5b8a3becfd615f
orieasy1 <easy1nhard2@gmail.com>
2024-03-04 15:21:43 +0900

    pull

--- Java-Basic-Code/src/class1/ClassStart2.java
+++ Java-Basic-Code/src/class1/ClassStart2.java
@@ -1,18 +1,14 @@
 package class1;
 
-public class ClassStart1 {
+public class ClassStart2 {
     public static void main(String[] args) {
-        //이렇게 작성을 하면 학생 수가 늘어날 때 마다 코드가 길어지고 복잡해진다.
-        String student1Name = "학생1";
-        int student1Age = 15;
-        int student1Grade = 90;
+        String[] studentNames = {"학생1", "학생2"};
+        int[] studentAges = {15, 16};
+        int[] studentGrades = {90, 80};
 
-        String student2Name = "학생2";
-        int student2Age = 16;
-        int student2Grade = 80;
-
-        System.out.println("이름:" + student1Name + " 나이:" + student1Age + " 성적:" + student1Grade);
-        System.out.println("이름:" + student2Name + " 나이:" + student2Age + " 성적:" + student2Grade);
+        for(int i = 0; i < studentNames.length; i++) {
+            System.out.println("이름:" + studentNames[i] + " 나이:" + studentAges[i] + " 성적:" + studentGrades[i]);
+        }
 
     }
 }
--- Java-Basic-Code/src/class1/ClassStart3.java
+++ Java-Basic-Code/src/class1/ClassStart3.java
@@ -1,2 +1,18 @@
-package class1;public class ClassStart3 {
+package class1;
+
+public class ClassStart3 {
+    public static void main(String[] args) {
+        Student student1;
+        student1 = new Student();
+        student1.name = "학생1";
+        student1.age = 15;
+        student1.grade = 90;
+
+        Student student2;
+        student2 = new Student();
+        student2.name = "학생1";
+        student2.age = 15;
+        student2.grade = 90;
+    }
+
 }
